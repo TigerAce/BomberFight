3b70da63ad3b2059d7425c9f7b74183ef0a01cd5
Yunhao Chen <peter.cyh@hotmail.com>
2014-07-25 09:17:09 +1000

    add bomb class

---
+++ core/src/com/game/bomberfight/core/Bomb.java
@@ -0,0 +1,37 @@
+package com.game.bomberfight.core;
+
+import com.game.bomberfight.model.Explosive;
+
+public class Bomb extends Explosive{
+
+	protected Bomb(float x, float y, float time, float powerX, float powerY) {
+		super(x, y, time, powerX, powerY);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void explode(float x, float y, float powerX, float powerY) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
--- core/src/com/game/bomberfight/model/Explosive.java
+++ core/src/com/game/bomberfight/model/Explosive.java
@@ -2,13 +2,13 @@ package com.game.bomberfight.model;
 
 import com.game.bomberfight.interfaces.Explosible;
 
-public abstract class Bomb extends GameObject implements Explosible{
+public abstract class Explosive extends GameObject implements Explosible{
 	
 	protected float time;
 	protected float powerX;
 	protected float powerY;
 	
-	protected Bomb(float x, float y, float time, float powerX, float powerY){
+	protected Explosive(float x, float y, float time, float powerX, float powerY){
 		super(x, y);
 		this.time = time;
 		this.powerX = powerX;
