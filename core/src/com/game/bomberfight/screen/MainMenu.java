f8d35ce3a1086bac0353a674bec778174cd47222
Samruilio <samruilio@gmail.com>
2014-08-07 15:07:45 +1000

    re layout the mainmenu

--- core/src/com/game/bomberfight/screen/MainMenu.java
+++ core/src/com/game/bomberfight/screen/MainMenu.java
@@ -7,6 +7,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Container;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
@@ -16,8 +19,10 @@ import com.badlogic.gdx.scenes.scene2d.ui.List;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.SplitPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.game.bomberfight.model.GameInfo;
@@ -56,6 +61,11 @@ public class MainMenu implements Screen {
 	private Skin skin;
 	private Stage stage;
 	private GameInfo gameInfo;
+	private Table table;
+	
+	public String networkMode = "Localhost";
+	public String gameMode = "Normal";
+	public String nickname = "Kill La Kill";
 
 	public MainMenu() {
 		// TODO Auto-generated constructor stub
@@ -75,7 +85,7 @@ public class MainMenu implements Screen {
 		viewport.getCamera().update();
 		stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
 		stage.draw();
-		Table.drawDebug(stage);
+		//Table.drawDebug(stage);
 	}
 
 	@Override
@@ -85,114 +95,23 @@ public class MainMenu implements Screen {
 		viewport.update(width, height);
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public void show() {
 		skin = new Skin(Gdx.files.internal("data/uiskin.json"));
 		
-		Table table = new Table();
+		table = new Table();
 		table.setFillParent(true);
-		table.pad(5);
-		table.top();
-		table.left();
+		table.left().top().pad(5);
 		
-		Label mappreviewLabel = new Label("Map Preview:", skin);
-		table.add(mappreviewLabel).expandX().left().top().padBottom(5);
-		
-		Label networkLabel = new Label("Network:", skin);
-		table.add(networkLabel).left().top().padBottom(5).padRight(5);
-		
-		@SuppressWarnings("rawtypes")
-		SelectBox networkDropdown = new SelectBox(skin);
-		networkDropdown.setItems(networkEntries);
-		networkDropdown.setSelected("Localhost");
-		table.add(networkDropdown).expandX().fillX().left().top().padBottom(5);
-		
-		table.row();
-		
-		Image imageActor = new Image(new Texture(Gdx.files.internal("img/ui/metal_classic.png")));
-		final Container<Image> imageContainer = new Container<Image>();
-		imageContainer.setActor(imageActor);
-		ScrollPane previewScrollPane = new ScrollPane(imageContainer, skin);
-		table.add(previewScrollPane).minSize(400, 300).maxSize(400, 300).left().top().padBottom(5);
-		
-		Label gameModeLabel = new Label("Game Mode:", skin);
-		table.add(gameModeLabel).left().top().padBottom(5).padRight(5);
-		
-		@SuppressWarnings("rawtypes")
-		SelectBox gameModeDropdown = new SelectBox(skin);
-		gameModeDropdown.setItems(gameModeEntries);
-		gameModeDropdown.setSelected("Normal");
-		table.add(gameModeDropdown).expandX().fillX().left().top().padBottom(5);
-		
-		table.row();
-		
-		Label mapLabel = new Label("Map:", skin);
-		table.add(mapLabel).expandX().left().top().padBottom(5);
-		
-		table.row();
-		
-		@SuppressWarnings("rawtypes")
-		final List list = new List(skin);
-		list.setItems(mapEntries);
-		list.getSelection().setMultiple(false);
-		list.getSelection().setRequired(true);
-		gameInfo.gameMap = "img/tmx/ground2.tmx";
-		
-		ScrollPane scrollPane = new ScrollPane(list, skin);
-		scrollPane.setFlickScroll(true);
-		table.add(scrollPane).minWidth(400).fillY().left().top();
-		
-		TextButton startButton = new TextButton("Start", skin);
-		table.add(startButton).colspan(2).minSize(100, 50);
+		SplitPane splitPane = new SplitPane(createLeftSplit(), createRightSplit(), false, skin);
+		splitPane.setName("splitPane");
+		splitPane.setMinSplitAmount(0.5f);
+		splitPane.setMaxSplitAmount(0.500001f);
+		table.add(splitPane).fill().expand();
 		
 		table.pack();
 		stage.addActor(table);
-		//table.debug();
-		
-		scrollPane.addListener(new ChangeListener() {
-			@Override
-			public void changed(ChangeEvent event, Actor actor) {
-				// TODO Auto-generated method stub
-				String item = (String) list.getSelected();
-				if (item.equalsIgnoreCase("Metal Classic")) {
-					imageContainer.setActor(new Image(new Texture(Gdx.files
-							.internal("img/ui/metal_classic.png"))));
-					gameInfo.gameMap = "img/tmx/ground2.tmx";
-				} else if (item.equalsIgnoreCase("No Hide")) {
-					imageContainer.setActor(new Image(new Texture(Gdx.files
-							.internal("img/ui/no_hide.png"))));
-					gameInfo.gameMap = "img/tmx/ground1.tmx";
-				} else if (item.equalsIgnoreCase("Mud&Grass")) {
-					imageContainer.setActor(new Image(new Texture(Gdx.files
-							.internal("img/ui/mud_grass.png"))));
-					gameInfo.gameMap = "img/tmx/ground4.tmx";
-				} else {
-					imageContainer.setActor(null);
-					gameInfo.gameMap = null;
-				}
-			}
-		});
-		
-		startButton.addListener(new ChangeListener() {
-			
-			@Override
-			public void changed(ChangeEvent event, Actor actor) {
-				// TODO Auto-generated method stub
-				TextButton button = (TextButton) actor;
-				if (button.isPressed()) {
-					if (gameInfo.gameMap == null) {
-						Dialog dialog = new Dialog("Warning!", skin);
-						dialog.text("The map you selected is not available!").button("OK", true).key(Keys.ENTER, true)
-						.key(Keys.ESCAPE, false).show(stage);
-					} else {
-						LoadingScreen loadingScreen = new LoadingScreen();
-						loadingScreen.setGameInfo(gameInfo);
-						((Game) Gdx.app.getApplicationListener()).setScreen(loadingScreen);
-					}
-				}
-			}
-		});
+		table.debug();
 	}
 
 	@Override
@@ -218,6 +137,243 @@ public class MainMenu implements Screen {
 		// TODO Auto-generated method stub
 		stage.dispose();
 		skin.dispose();
+	}
+	
+	public Table createMapPreview() {
+		Table table = new Table();
+		table.setName("map preview");
+		table.debug();
+		
+		Label label = new Label("Map Preview:", skin);
+		label.setName("label");
+		table.add(label).left().top().expand();
+		
+		table.row();
+		
+		Image image = new Image(new Texture(Gdx.files.internal("img/ui/metal_classic.png")));
+		image.setName("image");
+		
+		Container<Image> container = new Container<Image>();
+		container.setName("container");
+		container.setActor(image);
+		
+		ScrollPane scrollPane = new ScrollPane(container, skin);
+		scrollPane.setName("scrollpane");
+		table.add(scrollPane).expand();
+		
+		return table;
+	}
+	
+	public Table createMapList() {
+		Table table = new Table();
+		table.setName("map list");
+		table.debug();
+		
+		Label label = new Label("Map:", skin);
+		label.setName("label");
+		table.add(label).left().expand();
+		
+		table.row();
+		
+		List<Object> list = new List<Object>(skin);
+		list.setName("list");
+		list.setItems(mapEntries);
+		list.getSelection().setMultiple(false);
+		list.getSelection().setRequired(true);
+		gameInfo.gameMap = "img/tmx/ground2.tmx";
+		
+		ScrollPane scrollPane = new ScrollPane(list, skin);
+		scrollPane.setName("scrollpane");
+		scrollPane.setFlickScroll(true);
+		table.add(scrollPane).left().expand().fill();
+		
+		scrollPane.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				// TODO Auto-generated method stub
+				if (actor instanceof List) {
+					@SuppressWarnings("unchecked")
+					List<Object> list = (List<Object>) actor;
+					String item = (String) list.getSelected();
+					Group parent = list.getParent();
+					while (!parent.getName().equalsIgnoreCase("splitPane")) {
+						parent = parent.getParent();
+					}
+					if (parent.getName().equalsIgnoreCase("splitPane")) {
+						Table table = parent.findActor("map preview");
+						ScrollPane scrollPane = table.findActor("scrollpane");
+						Container<Image> container = scrollPane.findActor("container");
+						if (item.equalsIgnoreCase("Metal Classic")) {
+							container.setActor(new Image(new Texture(Gdx.files
+									.internal("img/ui/metal_classic.png"))));
+							gameInfo.gameMap = "img/tmx/ground2.tmx";
+						} else if (item.equalsIgnoreCase("No Hide")) {
+							container.setActor(new Image(new Texture(Gdx.files
+									.internal("img/ui/no_hide.png"))));
+							gameInfo.gameMap = "img/tmx/ground1.tmx";
+						} else if (item.equalsIgnoreCase("Mud&Grass")) {
+							container.setActor(new Image(new Texture(Gdx.files
+									.internal("img/ui/mud_grass.png"))));
+							gameInfo.gameMap = "img/tmx/ground4.tmx";
+						} else {
+							container.setActor(null);
+							gameInfo.gameMap = null;
+						}
+					}
+				}
+			}
+		});
+		
+		return table;
+	}
+	
+	public Table createLeftSplit() {
+		Table table = new Table();
+		table.setName("left split");
+		table.debug();
+		
+		SplitPane splitPane = new SplitPane(createMapPreview(), createMapList(), true, skin);
+		splitPane.setName("splitPane");
+		splitPane.setMinSplitAmount(0.5f);
+		splitPane.setMaxSplitAmount(0.500001f);
+		table.add(splitPane).fill().expand();
+		
+		return table;
+	}
+	
+	public Table createNetworkMode() {
+		Table table = new Table();
+		table.setName("network mode");
+		table.debug();
+		
+		Label label = new Label("Network:", skin);
+		label.setName("label");
+		table.add(label).left().bottom().padRight(5).width(100);
+		
+		SelectBox<Object> selectBox = new SelectBox<Object>(skin);
+		selectBox.setItems(networkEntries);
+		selectBox.setSelected("Localhost");
+		table.add(selectBox).expandX().fillX();
+		
+		selectBox.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				// TODO Auto-generated method stub
+				@SuppressWarnings("unchecked")
+				SelectBox<Object> selectBox = (SelectBox<Object>) actor;
+				networkMode = (String) selectBox.getSelected();
+			}
+		});
+		
+		return table;
+	}
+	
+	public Table createGameMode() {
+		Table table = new Table();
+		table.setName("game mode");
+		table.debug();
+		
+		Label label = new Label("Game Mode:", skin);
+		label.setName("label");
+		table.add(label).left().bottom().padRight(5).width(100);
+		
+		SelectBox<Object> selectBox = new SelectBox<Object>(skin);
+		selectBox.setItems(gameModeEntries);
+		selectBox.setSelected("Normal");
+		table.add(selectBox).expandX().fillX();
+		
+		selectBox.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				// TODO Auto-generated method stub
+				@SuppressWarnings("unchecked")
+				SelectBox<Object> selectBox = (SelectBox<Object>) actor;
+				gameMode = (String) selectBox.getSelected();
+			}
+		});
+		
+		return table;
+	}
+	
+	public Table createStartButton() {
+		Table table = new Table();
+		table.setName("start button");
+		table.debug();
+		
+		TextButton textButton = new TextButton("Start", skin);
+		table.add(textButton);
+		
+		textButton.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				// TODO Auto-generated method stub
+				TextButton button = (TextButton) actor;
+				if (button.isPressed()) {
+					if (gameInfo.gameMap == null) {
+						showDialog("Warning!", "The map you selected is not available!");
+					} else {
+						if (!networkMode.equalsIgnoreCase("Localhost") && nickname.isEmpty()) {
+							showDialog("Warning!", "Please input your nickname so that your friends can talk to you!");
+						} else {
+							LoadingScreen loadingScreen = new LoadingScreen();
+							loadingScreen.setGameInfo(gameInfo);
+							((Game) Gdx.app.getApplicationListener()).setScreen(loadingScreen);
+						}
+					}
+				}
+			}
+		});
+		
+		return table;
+	}
+	
+	public Table createNickName() {
+		Table table = new Table();
+		table.setName("nickname");
+		table.debug();
+		
+		Label label = new Label("Nickname:", skin);
+		label.setName("label");
+		table.add(label).left().bottom().padRight(5).width(100);
+		
+		TextField textField = new TextField("Kill La Kill", skin);
+		table.add(textField).expandX().fillX();
+		
+		textField.addListener(new InputListener(){
+			public boolean keyTyped (InputEvent event, char character) {
+				if (event.getListenerActor() == event.getTarget()) {
+					TextField textField = (TextField) event.getListenerActor();
+					nickname = textField.getText();
+					return true;
+				}
+				return false;
+			}
+		});
+		
+		return table;
+	}
+	
+	public Table createRightSplit() {
+		Table table = new Table();
+		table.setName("right split");
+		table.debug();
+		
+		table.defaults().padBottom(5);
+		table.add(createNetworkMode()).expandX().fillX();
+		table.row();
+		table.add(createGameMode()).expandX().fillX();
+		table.row();
+		table.add(createNickName()).expandX().fillX();
+		table.row();
+		table.add(createStartButton()).expandY().bottom().padBottom(50);
+		
+		return table;
+	}
+	
+	public void showDialog(String caption, String text) {
+		Dialog dialog = new Dialog(caption, skin);
+		dialog.text(text).button("OK", true).key(Keys.ENTER, true)
+		.key(Keys.ESCAPE, false).show(stage);
 	}
 
 }
